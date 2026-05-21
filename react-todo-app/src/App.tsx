05644effa7fe82e71ec9895c4bbea0fb62a9c39c
Oscar Di Lenarda <oscar@dilenarda.com>
2026-05-21 23:41:45 +0200

    add missing steps in react project

--- react-todo-app/src/App.tsx
+++ react-todo-app/src/App.tsx
@@ -2,6 +2,21 @@ import TodoList from './components/TodoList'
 import './App.css'
 
 export default function App() {
+  /*
+    1. Dans le même projet, créez un composant TodoList minimal :
+        - Affichez une liste de tâches (todo list) gérée avec useState()
+        - Ajoutez un champ pour saisir une nouvelle tâche
+
+    2. Ajoutez un en-tête (header) dans TodoList :
+        - Affichez le nombre d’items dans la liste
+        - Ajoutez un bouton pour trier les tâches par longueur de phrase
+
+    3. Pour chaque todo, affichez un chat aléatoire (image) au chargement du composant :
+        - Utilisez l’API https://cataas.com/cat?json=true pour obtenir une image de chat
+
+    => Modifiez ou remplacez le composant TodoList en dessous pour répondre à ces objectifs.
+  */
+
   return (
     <TodoList />
   )
